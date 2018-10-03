fb15f434463197073fc493083c747048ac2df99d
pharzan <farzan.tinati@gmail.com>
2018-10-03 22:47:39 +0330

    added base app and users db creation process

--- api.py
+++ api.py
@@ -4,5 +4,17 @@ from flask_cors import CORS, cross_origin
 from flask import jsonify
 
 from db import DB
-import variables
+import variables, queries
 
+
+app = Flask(__name__, static_folder="./static",
+            template_folder="./templates/")
+api = Api(app)
+
+
+db_filename = variables.db_filename
+db = DB(db_filename, queries.users_table)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
